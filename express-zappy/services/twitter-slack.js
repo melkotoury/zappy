f2b2df90dae63c4ecd0a90c8ec2b5ed07c10bda0
melkotoury <melkotoury@gmail.com>
2018-04-11 07:05:18 +0200

    refact: slack-twitter service

--- express-zappy/services/twitter-slack.js
+++ express-zappy/services/twitter-slack.js
@@ -29,39 +29,10 @@ module.exports = {
         // Get our API
         const api = require('./../routes/api.js');
 
-        // rtm.on('message', (event) => {
-        //     let text = event.text? event.text.replace(/[^a-zA-Z ]/g, ""): '';
-        //     text = text? ` ${text} `:'';
-        //
-        //     if(_.contains(slackCred.marketChannels, event.channel) && text && /\sgo\s/i.test(text)){
-        //         // call twitter api for fetching data
-        //         client.get('statuses/user_timeline', {count: 200}, async function(error, tweets, response) {
-        //             let tweetsLen = tweets.length;
-        //             if (!error) {
-        //                 for(let index = 0; index < tweetsLen; index++){
-        //                     let tweet = tweets[index];
-        //                     let twt = await api.findTweet(tweet.id_str);
-        //                     if(!twt){
-        //                         let tweetSaved = await api.saveTweet({id: tweet.id_str, text: tweet.text, date: tweet.created_at});
-        //                         // push tweets to the client after be updated
-        //                         if(tweetSaved && index == tweetsLen - 1){
-        //                             api.pushTweets();
-        //                         }
-        //                     } else {
-        //                         // push tweets to the client after be updated
-        //                         if(index == tweetsLen - 1){
-        //                             api.pushTweets();
-        //                         }
-        //                     }
-        //                 }
-        //             }
-        //         });
-        //     }
-        // });
         rtm.on('message', (event) => {
             let text = event.text? event.text.replace(/[^a-zA-Z ]/g, ""): ''
             text = text? ` ${text} `:''
-            if(event.text === 'hello') {
+            if(event.text === "go") {
                 // call twitter api for fetching data
                 client.get('statuses/user_timeline', {count: 200}, async function(error, tweets, response) {
                     let tweetsLen = tweets.length;
@@ -81,13 +52,12 @@ module.exports = {
                                 //     ]
                                 // })
                                 // push tweets to the client after be updated
-                                if(tweetSaved && index == tweetsLen - 1){
-
+                                if(tweetSaved && index === tweetsLen-1){
                                     api.pushTweets();
                                 }
                             } else {
                                 // push tweets to the client after be updated
-                                if(index == tweetsLen - 1){
+                                if(index === tweetsLen-1){
                                     // web.chat.postMessage({
                                     //     channel: event.channel,
                                     //     text: tweet.text ,
